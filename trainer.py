755af704c08e70640d7084d4fa22d00a2a1cd6dd
s108021132 <your_email_address>
2022-08-15 00:42:18 +0800

    finish decomposition

--- trainer.py
+++ trainer.py
@@ -353,10 +353,7 @@ def estimate_ranks(layer):
     be performed on, and estimates the ranks of the matrices using VBMF 
     """
 
-    weights = layer.weight.data
-    weights = weights.numpy()
-    print(weights)
-    print(type(weights))
+    weights = layer.weight.data.numpy()
     unfold_0 = tl.base.unfold(weights, 0) 
     unfold_1 = tl.base.unfold(weights, 1)
     _, diag_0, _, _ = VBMF.EVBMF(unfold_0)
@@ -375,7 +372,7 @@ def tucker_decomposition_conv_layer(layer):
     ranks = estimate_ranks(layer)
     print(layer, "VBMF Estimated ranks", ranks)
     core, [last, first] = \
-        partial_tucker(layer.weight.data, \
+        partial_tucker(layer.weight.data.numpy(), \
             modes=[0, 1], rank=ranks, init='svd')
 
     # A pointwise convolution that reduces the channels from S to R3
@@ -395,15 +392,29 @@ def tucker_decomposition_conv_layer(layer):
         out_channels=last.shape[0], kernel_size=1, stride=1,
         padding=0, dilation=layer.dilation, bias=True)
 
-    last_layer.bias.data = layer.bias.data
+    if layer.bias != None:
+        last_layer.bias.data = layer.bias.data
 
     first_layer.weight.data = \
-        torch.transpose(first, 1, 0).unsqueeze(-1).unsqueeze(-1)
-    last_layer.weight.data = last.unsqueeze(-1).unsqueeze(-1)
-    core_layer.weight.data = core
+        torch.transpose(torch.from_numpy(first.copy()), 1, 0).unsqueeze(-1).unsqueeze(-1)
+    last_layer.weight.data = torch.from_numpy(last.copy()).unsqueeze(-1).unsqueeze(-1)
+    core_layer.weight.data = torch.from_numpy(core.copy())
 
     new_layers = [first_layer, core_layer, last_layer]
     return nn.Sequential(*new_layers)
+
+
+def get_layer(model,name):
+    layer = model
+    for attr in name.split('.'):
+        layer = getattr(layer,attr)
+    return layer
+
+
+def set_layer(model,name,layer):
+    attr,name = name.rsplit('.',1)
+    model = get_layer(model,attr)
+    setattr(model,name,layer)
 
 
 def decomposition():
@@ -420,15 +431,25 @@ def decomposition():
     #model = checkpoint
     model.eval()
     model.cpu()
-    
-    for layers in model.modules():
-        for name,layer in layers.named_modules():
+    print(model)
+    #new_model = model.copy()
+    decomposed_layer = []
+    for name,layer in model.named_modules():
+        #for i,(name,layer) in enumerate(layers.named_modules()):
             if isinstance(layer, torch.nn.modules.conv.Conv2d):
                 conv_layer = layer
-                #print(layer)
                 decomposed = tucker_decomposition_conv_layer(conv_layer)
-                setattr(model,name,nn.Sequential(*decomposed))
-        torch.save(model, 'decomposed_model')
+                #setattr(layers,name,nn.Sequential(*decomposed))
+                
+                decomposed_layer.append((name,decomposed))
+                
+        #torch.save(model.state_dict(), 'decomposed_model.pt')
+    print(decomposed_layer)
+    decomposed_layer.pop(0)
+    for name,layer in decomposed_layer:
+        set_layer(model,name,layer)
+    torch.save(model.state_dict(), 'decomposed_model.pt')
+    print(model)
 
 
 if __name__ == '__main__':
